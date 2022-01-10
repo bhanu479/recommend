e6698d002d127bec517193d2888b9a5cd8f82de8
Bhanu pratap singh shekhawat <51327892+bhanu479@users.noreply.github.com>
2022-01-10 15:07:42 +0530

    cleared errors

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, redirect
-import pickle
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 movies_data = pd.read_csv("movies_data.csv")
 movies = pd.read_csv("movies_tags.csv")
-cv = CountVectorizer(max_features=5000,stop_words='english')
-vectors = cv.fit_transform(movies.tags).toarray()
-similarity = cosine_similarity(vectors)
-
+def create_similarity(data):
+    cv = CountVectorizer(max_features=5000,stop_words='english')
+    vectors = cv.fit_transform(data.tags).toarray()
+    similarity = cosine_similarity(vectors)
+    return similarity
 def recommender(movie):
+    similarity = create_similarity(movies)
     movie_index = movies[movies['title']==movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]

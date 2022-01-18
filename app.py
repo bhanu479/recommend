a1ea4c213a6bde19cfc601bda3f78c92df5b76ec
Bhanu pratap singh shekhawat <51327892+bhanu479@users.noreply.github.com>
2022-01-18 21:30:59 +0530

    added 2nd page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 movies_data = pd.read_csv("movies_data.csv")
 movies = pd.read_csv("movies_tags.csv")
+poster = pd.read_csv("poster.csv")
 def create_similarity(data):
     cv = CountVectorizer(max_features=5000,stop_words='english')
     vectors = cv.fit_transform(data.tags).toarray()
@@ -34,10 +35,14 @@ def predict():
     if request.method == 'POST':
         mov = str(request.form['mov'])
         y = recommender(mov)
-        movie_index = movies_data[movies_data['title']==mov].index[0]
-        search_movie = movies_data.iloc[movie_index]
-        return render_template("recommended.html",y=y,title = search_movie.title,overview = search_movie.overview,genres=search_movie.genres,cast=search_movie.cast,crew=search_movie.crew)
-
+        poster_index = poster[poster['id']==search_movie.id].index[0]
+        poster_data = poster.iloc[poster_index]
+        recommender_index=[]
+        recommender_data = pd.DataFrame()
+        for i in y.id:
+            recommender_index = poster[poster['id']==i].index[0]
+            recommender_data= recommender_data.append(poster.iloc[recommender_index],ignore_index=True)
+        return render_template("recommended.html",title = search_movie.title,overview = search_movie.overview,genres=search_movie.genres,cast=search_movie['cast'].split(","),crew=search_movie.crew,recommended_id=y.id,recommended_movies=y.movies,cast_poster=poster_data.cast.split(","),crew_poster=poster_data.crew,movie_poster=poster_data.poster,recommender_poster=recommender_data.poster)
     else:
         return render_template('index.html')
 

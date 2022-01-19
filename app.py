8c81a08a99fc7057a5584ea6776d7fdae8764b3e
Bhanu pratap singh shekhawat <51327892+bhanu479@users.noreply.github.com>
2022-01-19 19:38:04 +0530

    Server error fixed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-movies_data = pd.read_csv("movies_data.csv")
+movies_data = pd.read_csv("main_data.csv")
 movies = pd.read_csv("movies_tags.csv")
 poster = pd.read_csv("poster.csv")
 def create_similarity(data):
@@ -40,11 +40,9 @@ def predict():
         search_movie = movies_data.iloc[movie_index]
         poster_index = poster[poster['id']==search_movie.id].index[0]
         poster_data = poster.iloc[poster_index]
-        recommender_index=[]
         recommender_data = pd.DataFrame()
         for i in y.id:
-            recommender_index = poster[poster['id']==i].index[0]
-            recommender_data= recommender_data.append(poster.iloc[recommender_index],ignore_index=True)
+            recommender_data= recommender_data.append(poster[poster['id']==i],ignore_index=True)
         return render_template("recommended.html",title = search_movie.title,overview = search_movie.overview,genres=search_movie.genres,cast=search_movie['cast'].split(","),crew=search_movie.crew,recommended_id=y.id,recommended_movies=y.movies,cast_poster=poster_data.cast.split(","),crew_poster=poster_data.crew,movie_poster=poster_data.poster,recommender_poster=recommender_data.poster)
     else:
         return render_template('index.html')
